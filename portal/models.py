ca8d911c7f40675ed5fae513b54937ead90ebb5f
nveiseh <nveiseh@phoenixllg.com>
2013-04-23 12:22:40 -0400

    With Implmented Unit Tests on Portal

--- portal/models.py
+++ portal/models.py
@@ -21,7 +21,9 @@ class Poll(models.Model):
         return self.pub_date
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        now = timezone.now()
+        return now > self.pub_date >= now - datetime.timedelta(days = 1)
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
     
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
--- portal/tests.py
+++ portal/tests.py
@@ -6,6 +6,30 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+
+from portal.models import Poll, Choice
+
+class PollMethodTests(TestCase):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        def test_was_published_recently_with_future_poll(self):
+            #First we create the object we expect to fail
+            future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+            #Then, we test that object, to see if the object's function returns the correct value
+            self.assertEqual(future_poll.was_published_recently(), False)
+        
+        def test_was_published_recently_old_poll(self):
+            old_poll = Poll(pub_date = timezone.now() - datetime.timedelta(days=30))
+            self.assertEqual(old_poll.was_published_recently(), False)
+        
+        def test_was_published_recently_recent(self):
+            recent_poll = Poll(pub_date = timezone.now() - datetime.timedelta(hours=1))
+            self.assertEqual(recent_poll.was_published_recently(), True, "HORRAY?!")
+    
 
 
 class SimpleTest(TestCase):
