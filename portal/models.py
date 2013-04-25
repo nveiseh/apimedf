20b754388db6f9237c2ba134dcecb6837013f9f3
nveiseh <nveiseh@phoenixllg.com>
2013-04-25 10:35:12 -0400

    Docs Folder Added

--- portal/models.py
+++ portal/models.py
@@ -33,7 +33,35 @@ class Poll(models.Model):
         #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         
     #EXTENTIONS: poll_id will refer to the id number of the enterprise
+    #Define variables within the class, that are unique to each Enterprise object, ie company
+    ent_name = models.CharField(max_length = 200)
+    inst_date = models.DateTimeField('Date of Instantation with MEDF')
+    country = models.CharField(max_length = 100)
+    country_code = models.CharField(max_length = 3)
+    manager = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 200)
+    biz_type = models.CharField(max_length = 100)
+    biz_code = models.IntegerField(default = 0 )
     
+    #Define Functions to return parts of object when called
+    def get_country(self):
+        return self.country
+    
+    def get_country_code(self):
+        return self.country_code
+    
+    def get_manager(self):
+        return self.manager
+    
+    def get_address(self):
+        return self.address
+    
+    def get_biz_address(self):
+        return self.biz_type
+    
+    def get_biz_code(self):
+        return self.biz_code
+
     
     
 class Choice(models.Model):
@@ -46,4 +74,24 @@ class Choice(models.Model):
     
     def whatis_votes(self):
         return self.votes
+    
+    #EXTENTIONS: poll_id will refer to the id number of the enterprise
+    booking_date = models.DateTimeField('Date when Book Entry made with MEDF')
+    
+    revenue = models.FloatField()
+    cogs = models.FloatField()
+    depreciation = models.FloatField()
+    interestpaidondebt = models.FloatField()
+    interestearnedoncash = models.FloatField()
+    profitbeforetax = models.FloatField()
+    taxes = models.FloatField()
+    profitaftertax = models.FloatField()
+    dividends = models.FloatField()
+    retainedearnings = models.FloatField()
+    
+    totalinventory = models.FloatField()
+    totalwagespaid = models.FloatField()
+    gacostspaid = models.FloatField()
+    employeecount = models.IntegerField()
+    transactioncount = models.IntegerField()
     
